022ed28f6de5f6b5a627174917f99b558a54fbfc
Anstasia Cherkasova <nari_5@mail.ru>
2021-02-28 09:38:49 -0500

    fixed

--- HW_Python/Task5.py
+++ HW_Python/Task5.py
@@ -1,32 +1,30 @@
 def listMAX(answer):
-    notint=[]
-    new_answer=[]
-    c=0
+    notint = []
+    new_answer = []
     for num in answer:
-        if(type(num)!=int):
+        if(type(num) != int):
             notint.append(num)
-            c=1
-    if c==1:
-        print("You've passed some extra elements that I can't parse: ",end=' ')
+    if len(notint) != 0:
+        print("You've passed some extra elements that I can't parse: ",end = ' ')
         return notint
     else:
         answer.sort(reverse = True)
-        new_answer=answer[:3]
+        new_answer = answer[:3]
     return(new_answer)
 
 print("Example 1:")
-list1=[1,234,3,77,96,54,33,1111]
+list1 = [1,234,3,77,96,54,33,1111]
 print(listMAX(list1))
 print("Example 2:")
-list1=[1,2,3,66.25,96,54.88,33]
+list1 = [1,2,3,66.25,96,54.88,33]
 print(listMAX(list1))
 print("Example 3:")
-list1=[1,2,3,'hola',96,54,33]
+list1 = [1,2,3,'hola',96,54,33]
 print(listMAX(list1))
 print("Example 4:")
-list1=[3,96]
+list1 = [3,96]
 print(listMAX(list1))
 print("Example 5:")
-list1=[9]
+list1 = [9]
 print(listMAX(list1))
 
--- HW_Python/Task8.1.py
+++ HW_Python/Task8.1.py
@@ -1,12 +1,16 @@
-ip={}
-arg=input('Give name of file:')
+ip = {}
+arg = input('Give name of file:')
 with open(arg) as f:
-    myList = [ line.split() for line in f ]   
-    for c in range(1, len(myList)):    
-        key=myList[c][0]
-        byte=int(myList[c][8])
-        val=ip.get(key,0)+byte
-        ip[key]=val
+    myList = [ line.split(' ') for line in f ]
+    for c in myList[1::2]:
+        if c != myList[-1]: 
+            key = c[0]
+            if c[9] == '-':
+                byte = 0
+            else:
+                byte = int(c[9])
+        val = ip.get(key,0)+byte
+        ip[key] = val
 for key, val in ip.items():
     print ('{} bytes for {}'.format(val, key))
 
